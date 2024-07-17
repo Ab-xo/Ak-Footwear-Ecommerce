9060bed80483af9ac5be0e6add4598f8a628bda6
Abraham Gebeyehu <118066569+Ab-xo@users.noreply.github.com>
2024-07-17 20:44:57 +0300

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -3,7 +3,7 @@
 $serverName = "localhost";
 $dBUsername = "root";
 $dBPassword = "";
-$dBName = "myphp_login";
+$dBName = "";
 
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
